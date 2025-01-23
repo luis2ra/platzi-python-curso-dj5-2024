62203c0aedd453620535b17a4b75512686c270b6
Luis Altuve <laltuve@ext.sebbusiness.com>
2025-01-23 10:20:42 -0500

    ajusted DATABASES variable of settings

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -81,8 +81,18 @@ WSGI_APPLICATION = "coffee_shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {"default": env.db("DJANGO_DB_URL")}
+# DATABASES = {"default": env.db("DJANGO_DB_URL")}
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "coffeeshop",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
